0bcbc8f61cc79afe12a25faf0abd31889d6dcf88
myrteza <myrteza@hotmail.fr>
2025-04-26 22:30:37 +0200

    rename oc-lettings-site to Oc Lettings Site

--- oc_lettings_site/apps.py
+++ oc_lettings_site/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 class OCLettingsSiteConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'oc_lettings_site'
+    verbose_name = 'Oc Lettings Site'
