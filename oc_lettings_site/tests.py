1823a60307aa0191e19a4a0739c388095269583c
myrteza <myrteza@hotmail.fr>
2025-04-27 14:58:56 +0200

    intall coverage, tests pass 94%, create htmlcov/index.html file to ckeck the cover in browser

--- oc_lettings_site/tests.py
+++ oc_lettings_site/tests.py
@@ -1,6 +1,36 @@
 """Tests for the oc_lettings_site app."""
 
 
-def test_dummy():
-    """Dummy test to verify the test framework setup."""
-    assert 1
+from django.test import TestCase
+from django.urls import reverse, resolve
+from oc_lettings_site import views
+
+class SiteViewsTest(TestCase):
+    """Tests for oc_lettings_site views."""
+
+    def test_index_view(self):
+        """Index view returns 200 and uses the correct template."""
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+
+    def test_custom_404_view(self):
+        """Custom 404 view returns 404 and uses the 404 template."""
+        # On simule une URL inconnue
+        response = self.client.get('/nonexistent-page/')
+        self.assertEqual(response.status_code, 404)
+        self.assertTemplateUsed(response, '404.html')
+
+
+class SiteURLsTest(TestCase):
+    """Tests for oc_lettings_site URL routing."""
+
+    def test_index_url_resolves_to_index_view(self):
+        """The root URL should resolve to oc_lettings_site.views.index."""
+        resolver = resolve('/')
+        self.assertEqual(resolver.func, views.index)
+
+    def test_404_handler_is_custom(self):
+        """handler404 must be set to custom_404."""
+        import oc_lettings_site.urls as urls_mod
+        self.assertEqual(urls_mod.handler404, 'oc_lettings_site.views.custom_404')
