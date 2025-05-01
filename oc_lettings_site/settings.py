5e454adc9195b77c149b2eb14fd9f8a29da0bcf3
myrteza <myrteza@hotmail.fr>
2025-05-01 15:20:55 +0200

    Make DEBUG and ALLOWED_HOSTS configurable via environment

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -19,13 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# SECURITY WARNING: don't run with debug turned on in production!*
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1'
-]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 
 # Application definition
